9ba458cfaa669bc1f577e79caa494ebe766d3f4e
Yikey <gtc122004@gmail.com>
2018-02-22 20:49:47 -0500

    Update gamerole.js

--- server/commands/gamerole.js
+++ server/commands/gamerole.js
@@ -12,19 +12,20 @@ module.exports = {
 			let game = game.toLowerCase();
 			if (games.find((value) => {return value == game;}) !== null) {
 				if (call.message.member.roles.find("name", `${game}`)) {
-					call.message.member.removeRole(message.guild.roles.find("name", game));
-					call.message.channel.send(`Since you already had the \`${game}\` game role, it has been removed from you!`)
+					call.message.member.removeRole(call.message.guild.roles.find("name", game));
+					call.message.channel.send(`Since you already had the \`${game}\` game role,
+					 it has been removed from you!`);
 				} else {
-					call.message.member.addRole(message.guild.roles.find("name", game));
-					call.message.channel.send(`Successfully given you the \`${game}\` game role!`)
+					call.message.member.addRole(call.message.guild.roles.find("name", game));
+					call.message.channel.send(`Successfully given you the \`${game}\` game role!`);
 				}
 			} else {
 				call.message.channel.send(`${game} is not a valid game. A list of games can be found running
 				the MantaroBot command \`.gameroles\`. If it is a valid game and you would  like to see it
-				here in the near future, dm \`@ethanlaj#8805\`.`)
+				here in the near future, dm \`@ethanlaj#8805\`.`);
 			}
 		} else {
-			call.message.channel.send("You didn't ask for any game role. \:( \n Usage: `/gamerole (gamname)`")
+			call.message.channel.send("You didn't ask for any game role. \n Usage: `/gamerole (gamname)`");
 		}
 	}
- };
+};
