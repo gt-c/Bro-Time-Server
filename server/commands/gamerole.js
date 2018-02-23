2b9daa33d63390a14f45fcf8966b222ba94c6ad6
Yikey <gtc122004@gmail.com>
2018-02-22 22:54:23 -0500

    Update gamerole.js

--- server/commands/gamerole.js
+++ server/commands/gamerole.js
@@ -9,18 +9,18 @@ module.exports = {
 	execute: (call) => {
 		let game = call.params.readRaw(" ").toLowerCase();
 		if (game !== null) {
- 			if (games.includes(game)) {
+			if (games.includes(game)) {
 				if (call.message.member.roles.find("name", game)) {
 					call.message.member.removeRole(call.message.guild.roles.find("name", game));
 					call.message.channel
-					.send(`Since you already had the \`${game}\` game role, it has been removed from you!`);
+						.send(`Since you already had the \`${game}\` game role, it has been removed from you!`);
 				} else {
 					call.message.member.addRole(call.message.guild.roles.find("name", game));
 					call.message.channel.send(`Successfully given you the \`${game}\` game role!`);
 				}
 			} else {
 				call.message.channel
-				.send(`${game} is not a valid game option. Make sure it is an actual gamerole from \`.gameroles\`.`);
+					.send(`${game} is not a valid game option. Make sure it is an actual gamerole from \`.gameroles\`.`);
 			}
 		} else {
 			call.message.channel.send("You didn't ask for any game role. \n Usage: `/gamerole (gamname)`");
