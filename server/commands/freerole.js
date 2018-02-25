6f44754e45e56541fc461bea3e353eb6ce198678
Yikey <gtc122004@gmail.com>
2018-02-24 21:03:21 -0500

    Create freerole.js
    allows you to toggle being in one of the three free roles in the Bro Time server.

---
+++ server/commands/freerole.js
@@ -0,0 +1,26 @@
+var joinableroles = ["[F] QOTD", "[F] ANN", "[F] GW"];
+
+module.exports = {
+	id: "freerole",
+	load: () => {},
+	execute: (call) => {
+		var finput = "[F] "+call.params.readRaw().toUpperCase();
+		if (joinableroles.includes(finput)) {
+			if (call.message.member.roles.find("name", finput)) {
+				call.message.member.removeRole(call.message.guild.roles.find("name", `${finput}`)).then(() => {
+					call.message.channel.send(`Successfully removed the \`${finput}\` free role from you.`);
+				}).catch(() => {
+					call.message.channel.send(`Unable to remove the \`${finput}\` free role from you!`);
+				});
+			} else {
+				call.message.member.addRole(call.message.guild.roles.find("name", `${finput}`)).then(() => {
+					call.message.channel.send(`Successfully added the \`${finput}\` free role to you.`);
+				}).catch(() => {
+					call.message.channel.send(`Unable to add the \`${finput}\` free role to you!`);
+				});
+			}
+		} else {
+			call.message.channel.send(`\`${call.params.readRaw()}\` is not a valid freerole option`)
+		}
+	}
+};
