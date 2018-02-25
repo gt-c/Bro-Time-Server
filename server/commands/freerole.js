e6a8e530748d35bebf29d3965588e51d4f0d5062
Yikey <gtc122004@gmail.com>
2018-02-24 21:49:32 -0500

    Update freerole.js

--- server/commands/freerole.js
+++ server/commands/freerole.js
@@ -5,23 +5,25 @@ module.exports = {
 	load: () => {},
 	execute: (call) => {
 		var role = call.params.readRole(true, (candidate) => { return candidate.name.startsWith("[F] "); });
-		var finput = "[F] "+call.params.readRaw().toUpperCase();
-		if (joinableroles.includes(finput)) {
-			if(call.message.member.roles.has(role.id)) {
-				call.message.member.removeRole(role).then(() => {
-					call.message.channel.send(`Successfully removed the \`${finput}\` free role from you.`);
-				}).catch(() => {
-					call.message.channel.send(`Unable to remove the \`${finput}\` free role from you!`);
-				});
+		if (role !== null) {
+			var finupt = role.name
+			if (joinableroles.includes(finput)) {
+				if(call.message.member.roles.has(role.id)) {
+					call.message.member.removeRole(role).then(() => {
+						call.message.channel.send(`Successfully removed the \`${finput}\` free role from you.`);
+					}).catch(() => {
+						call.message.channel.send(`Unable to remove the \`${finput}\` free role from you!`);
+					});
+				} else {
+					call.message.member.addRole(role).then(() => {
+						call.message.channel.send(`Successfully added the \`${finput}\` free role to you.`);
+					}).catch(() => {
+						call.message.channel.send(`Unable to add the \`${finput}\` free role to you!`);
+					});
+				}
 			} else {
-				call.message.member.addRole(role).then(() => {
-					call.message.channel.send(`Successfully added the \`${finput}\` free role to you.`);
-				}).catch(() => {
-					call.message.channel.send(`Unable to add the \`${finput}\` free role to you!`);
-				});
+				call.message.channel.send(`\`${call.params.readRaw()} \` is not a valid freerole option`);
 			}
-		} else {
-			call.message.channel.send(`\`${call.params.readRaw()} \` is not a valid freerole option`);
 		}
 	}
 };
