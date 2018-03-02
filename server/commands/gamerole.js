cf4023388688ffc3205f8ccd48beb4deb86bd175
Yikey <gtc122004@gmail.com>
2018-03-02 16:24:35 -0500

    allows usage of partial role to obtain role

--- server/commands/gamerole.js
+++ server/commands/gamerole.js
@@ -1,7 +1,10 @@
-var games = ["roblox", "minecraft", "cuphead", "fortnite", "undertale", "unturned", "vrchat",
-	"pubg", "fnaf", "clash of clans", "clash royale", "sims", "terraria", "subnautica", "rocket league",
-	"portal", "hat in time", "csgo", "splatoon", "mario", "starbound", "garry's mod", "overwatch",
-	"call of duty", "destiny"];
+var games = ["<@&405163385148276736>", "<@&405163429188337665>", "<@&405163430056558592>", "<@&405163474335956994>", 
+	"<@&405163485463314432>", "<@&405163534285275146>", "<@&405163718226346007>", "<@&405163735989354496>",
+	"<@&405163837092790272>", "<@&405163788975996928>", "<@&405163655840268288>", "<@&405163938834153493>", 
+	"<@&405163996120088599>", "<@&405164035034578949>", "<@&405164088495439873>", "<@&405164127401803800>", 
+	"<@&405164166370820106>", "<@&405164202811064321>", "<@&405164240656138240>", "<@&405164283828109323>", 
+	"<@&405164323284058133>", "<@&405166298985922600>", "<@&410216775733739520>", "<@&410217263980085248>",
+	"<@&414507850136682498>"];
 
 module.exports = {
 	id: "gamerole",
@@ -10,7 +13,7 @@ module.exports = {
 		var prms = call.params;
 		var role = prms.readRole();
 		var rawinput = call.message.content.substr(10);
-		if (games.includes(rawinput.toLowerCase())) {
+		if (games.includes(role)) {
 			if (rawinput.toLowerCase() == "clash of clans") {
 				// temporary fix for the readRole error taking action on clash royale when the user specifies clash of clans
 				role = call.message.guild.roles.find("name", "Clash of Clans");
